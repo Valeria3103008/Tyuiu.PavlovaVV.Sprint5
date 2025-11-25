6557deea81e12fec36102254513da8453afe9041
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-11-25 17:54:39 +0500

    Task2

---
+++ Tyuiu.PavlovaVV.Sprint5.Task2.V30.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using Tyuiu.PavlovaVV.Sprint5.Task2.V30.Lib;
+namespace Tyuiu.PavlovaVV.Sprint5.Task2.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.PavlovaVV.Sprint5.Task2.V30/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.PavlovaVV.Sprint5.Task2.V30.Lib;
+namespace Tyuiu.PavlovaVV.Sprint5.Task2.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            DataService ds = new DataService();
+            int[,] mtrx = new int[3, 3] { { 3, -1, -3 },
+                                         {-2, -5, 0 },
+                                         { -8, -7, 2 }};
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]}\t");
+                }
+                Console.WriteLine();
+                Console.WriteLine("***************************************************************************");
+                Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+                Console.WriteLine("***************************************************************************");
+
+                string res = ds.SaveToFileTextData(mtrx);
+
+                Console.WriteLine("Файл: " + res);
+                Console.WriteLine("Создан!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
