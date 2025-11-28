50a38190171a244de1645b6cd6074c3f25a36e1f
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-11-28 19:10:11 +0500

    Task7

---
+++ Tyuiu.PavlovaVV.Sprint5.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Tyuiu.PavlovaVV.Sprint5.Task7.V5.Lib;
+namespace Tyuiu.PavlovaVV.Sprint5.Task7.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask7V5.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.PavlovaVV.Sprint5.Task7.V5/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.PavlovaVV.Sprint5.Task7.V5.Lib;
+namespace Tyuiu.PavlovaVV.Sprint5.Task7.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+
+            string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask7V5.txt";
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutDataFileTask7V5.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("Находится в файле: ");
+            pathSaveFile = ds.LoadDataAndSave(path);
+            Console.WriteLine("Все латинские буквы из файла были удалены = " + pathSaveFile);
+            Console.ReadKey();
+        }
+    }
+}
